5658b34fb0fabbd8e0d8613f980919e593915144
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-10-25 13:49:11 +0900

    fix: 빗방울 프리팹 order를 1로 변경, feat: -5점인 빗방울 추가

--- Assets/Scripts/rain.cs
+++ Assets/Scripts/rain.cs
@@ -11,10 +11,10 @@ public class rain : MonoBehaviour
     void Start()
     {
         float x = Random.Range(-2.7f, 2.7f);
-        float y = Random.Range(3.0f, 5.0f);
+        float y = Random.Range(6.0f, 7.0f);
         transform.position = new Vector3(x, y, 0);
 
-        type = Random.Range(1, 4);
+        type = Random.Range(1, 5);
         if(type == 1)
         {
             size = 1.2f;
@@ -32,6 +32,12 @@ public class rain : MonoBehaviour
             size = 0.8f;
             score = 3;
             GetComponent<SpriteRenderer>().color = new Color(150 / 255f, 150 / 255f, 255 / 255f, 255 / 255f);
+        }
+        else if (type == 4)
+        {
+            size = 0.8f;
+            score = -5;
+            GetComponent<SpriteRenderer>().color = new Color(255 / 255.0f, 100.0f / 255.0f, 100.0f / 255.0f, 255.0f / 255.0f);
         }
         transform.localScale = new Vector3(size, size, 0.0f);
     }
